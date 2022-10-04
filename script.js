de5c0aaf08ad439db5e4689da57ffcf291137e3d
Felipe Fernandes <felipe.tavares.fernandes@gmail.com>
2022-10-04 16:56:17 -0300

    requirement 4 done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const colors = document.getElementsByClassName('color');
+const button = document.getElementById('button-random-color');
 
 function generateColor() {
   for (let index = 1; index < 4; index += 1) {
@@ -7,8 +8,20 @@ function generateColor() {
     for (let i = 0; i < 6; i += 1) {
       color += letters[Math.floor(Math.random() * 16)];
     }
-    colors[index].style = `background-color: ${color}`;
+    colors[index].style.backgroundColor = color;
+    localStorage.setItem(`colorPalette${index}`, color);
   }
 }
 
-generateColor();
+function localColor() {
+  if (localStorage.colorPalette1 != null) {
+    colors[1].style.backgroundColor = localStorage.colorPalette1;
+    colors[2].style.backgroundColor = localStorage.colorPalette2;
+    colors[3].style.backgroundColor = localStorage.colorPalette3;
+  } else {
+    generateColor();
+  }
+}
+
+localColor();
+button.addEventListener('click', generateColor);
